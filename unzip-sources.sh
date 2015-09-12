6ca75971a408813ee24048e2784dbaf4e8976f9f
Gian Vizzotto <gfvizzotto@gmail.com>
2015-09-12 15:37:46 -0300

    Adjusting script to install ckan 2.3.1

--- unzip-sources.sh
+++ unzip-sources.sh
@@ -7,13 +7,16 @@ echo "\$__dir=$__dir\n\n"
 
 set -v
 
-echo "ckan 2.4..."
+#echo "ckan 2.4..."
+echo "v2.3.1"
 cd $__dir/_src/
-cp $__dir/source-codes/ckan-2.4.0.zip $__dir/source-codes/ckanext-pages-master.zip .
-rm -rf $__dir/ckan-ckan-2.4.0
+#cp $__dir/source-codes/ckan-2.4.0.zip $__dir/source-codes/ckanext-pages-master.zip .
+cp $__dir/source-codes/release-v2.3.1.zip $__dir/source-codes/ckanext-pages-master.zip .
+#rm -rf $__dir/ckan-ckan-2.4.0
+rm -rf $__dir/ckan-release-v2.3.1
 rm -rf $__dir/ckanext-pages-master
 unzip -qo ckanext-pages-master.zip && rm ckanext-pages-master.zip
-unzip -qo ckan-2.4.0.zip && rm ckan-2.4.0.zip
+unzip -qo release-v2.3.1.zip && rm release-v2.3.1.zip
 echo "datapusher..."
 cp $__dir/source-codes/datapusher-stable.zip $__dir/_service-provider/
 rm -rf $__dir/_service-provider/datapusher
